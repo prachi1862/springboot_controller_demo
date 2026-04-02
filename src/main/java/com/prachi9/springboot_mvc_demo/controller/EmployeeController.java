69c89c76b3f529206a023d201aa04fb05d73e5a7
Prachi_Mehra <prachimehra866@gmail.com>
2026-04-02 19:25:53 +0530

    feat: added service layer,mapper model and updated controller to use service.

--- src/main/java/com/prachi9/springboot_mvc_demo/controller/EmployeeController.java
+++ src/main/java/com/prachi9/springboot_mvc_demo/controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.prachi9.springboot_mvc_demo.controller;
 
+import com.prachi9.springboot_mvc_demo.repositories.EmployeeRepository;
+import com.prachi9.springboot_mvc_demo.entities.EmployeeEntity;
 import com.prachi9.springboot_mvc_demo.services.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 import com.prachi9.springboot_mvc_demo.dto.EmployeeDTO;
@@ -9,6 +11,10 @@ import java.util.List;
 @RequestMapping(path="/employee")
 public class EmployeeController {
 
+//    @GetMapping(path = "/getsecretmsg")
+//    public String getSecretMsg() {
+//        return "Secret Message: gygsuise";
+//    }
 private final EmployeeService employeeService;
 
 public EmployeeController(EmployeeService employeeService) {
@@ -26,6 +32,12 @@ public EmployeeController(EmployeeService employeeService) {
         return employeeService.getAllEmployees();
 
     }
+
+//    @PostMapping()
+//    public String newEmployee()
+//    {
+//        return "Hi!!! from PostMapping";
+//    }
 
     @PostMapping()
     public EmployeeDTO createEmployee(@RequestBody EmployeeDTO inputEmployee)
--- src/main/java/com/prachi9/springboot_mvc_demo/services/EmployeeService.java
+++ src/main/java/com/prachi9/springboot_mvc_demo/services/EmployeeService.java
@@ -1,4 +1,39 @@
 package com.prachi9.springboot_mvc_demo.services;
 
+import com.prachi9.springboot_mvc_demo.dto.EmployeeDTO;
+import com.prachi9.springboot_mvc_demo.entities.EmployeeEntity;
+import com.prachi9.springboot_mvc_demo.repositories.EmployeeRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+    private final ModelMapper modelMapper;
+
+    public EmployeeService (EmployeeRepository employeeRepository, ModelMapper modelMapper) {
+        this.employeeRepository=employeeRepository;
+        this.modelMapper = modelMapper;
+    }
+    public EmployeeDTO getEmployeeByID(Long id) {
+       EmployeeEntity employeeEntity=employeeRepository.findById(id).orElse(null);
+       return modelMapper.map(employeeEntity, EmployeeDTO.class);
+    }
+
+    public List<EmployeeDTO> getAllEmployees() {
+         List<EmployeeEntity> employeeEntities= employeeRepository.findAll();
+         return employeeEntities
+                 .stream()
+                 .map(employeeEntity -> modelMapper.map(employeeEntity, EmployeeDTO.class))
+                 .collect(Collectors.toList());
+    }
+
+    public EmployeeDTO createEmployee(EmployeeDTO inputEmployee){
+        EmployeeEntity toSaveEmployee= modelMapper.map(inputEmployee, EmployeeEntity.class);
+        EmployeeEntity savedEmployee= employeeRepository.save(toSaveEmployee);
+        return modelMapper.map(savedEmployee, EmployeeDTO.class);
+    }
 }
